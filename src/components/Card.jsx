44b3a14f60be1ce362a7923dd027ec899a1714a6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-02-24 10:11:05 -0500

    Fix spacing above button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,12 @@ const Card = ({ icon, description, txtColor, buttonBackColor, buttonHoverBackCol
 
   return (
     <div className={`w-[327px] p-12 ${buttonBackColor} `}>
-      <img src={icon} alt={title} className="mb-9" />
-      <h2 className="font-bigShoulders font-bold text-[40px] text-very-light-gray my-[25px]">
+      <img src={icon} alt={title} className="mb-[35px]" />
+      <h2 className="font-bigShoulders font-bold text-[40px] text-very-light-gray mb-[25px]">
         {title}
       </h2>
       <p className="font-lexendDeca leading-card text-white/75">{description}</p>
-      <button className={`${txtColor} rounded-3xl bg-very-light-gray h-12 w-[146px] font-lexendDeca font-normal  mt-[25px] md:mt-[96px] hover:cursor-pointer hover:text-very-light-gray hover:border-2 hover:border-very-light-gray ${buttonHoverBackColor}`}>
+      <button className={`${txtColor} rounded-3xl bg-very-light-gray h-12 w-[146px] font-lexendDeca font-normal  mt-[25px] md:mt-[83px] hover:cursor-pointer hover:text-very-light-gray hover:border-2 hover:border-very-light-gray ${buttonHoverBackColor}`}>
       Learn More
       </button>
     </div>
